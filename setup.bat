61df26f7a313228b8b8143ed5200f35d990acfa9
Brandon Cooke <brandoncookedev@gmail.com>
2017-08-05 03:19:52 -0400

    ACTUAL fix for the Execution policy. Set policy to RemoteSigned on setup.bat. After that should be good

--- setup.bat
+++ setup.bat
@@ -1,24 +1,37 @@
 @ECHO OFF
-	
-ECHO SETTING EXECUTION POLICY
-
-ECHO EXECUTION POLICY SET TO UNRESTRICTED
-
 ECHO SETTING VARIABLES...
+SET STARTUP_DIR="C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 SET CUR_DIR="%cd%"
 SET PS_FILE=AutomatedSystemRestore.ps1
-SET STARTUP_DIR="C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy RemoteSigned -Force' -Verb RunAs}"
+
+REM ########################################
+
+REM ECHO SETTING EXECUTION POLICY
+REM set theValue=
+REM for /f "delims=" %%a in ('PowerShell.exe Get-ExecutionPolicy') do @set theValue=%%a
+
+REM ECHO EXECUTION POLICY IS %theValue%
+REM PAUSE
+REM IF NOT %theValue% = 'Restricted' ( PowerShell.exe -ExecutionPolicy Bypass -File %POLICY_FILE% )
+REM ECHO EXECUTION POLICY SET TO UNRESTRICTED
+
+REM ########################################
+
 
 SET WRAPPER_FILE=%STARTUP_DIR%\runWrapper.bat
-SET WRAPPER_CMD=PowerShell.exe -ExecutionPolicy Bypass -File %CUR_DIR%\%PS_FILE%
+SET WRAPPER_CMD=PowerShell.exe -File %CUR_DIR%\%PS_FILE% 
 
 ECHO WROTE VARIABLES.
+PowerShell.exe Get-ExecutionPolicy
 ECHO WRITTING FILE...
 
 TYPE nul>%WRAPPER_FILE%
 ECHO @ECHO OFF>>%WRAPPER_FILE%
 ECHO %WRAPPER_CMD%>>%WRAPPER_FILE%
+
+REM ########################################
 
 ECHO WROTE FILE %WRAPPER_FILE%...
 ECHO CONTENTS:
@@ -26,6 +39,3 @@ TYPE %WRAPPER_FILE%
 
 ECHO DONE.
 PAUSE
-
-:ExecutionPolicySetup
-	
